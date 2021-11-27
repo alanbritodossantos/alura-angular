b524e4ca962295d786f3fa916eaa32d6e391ad46
Alan Brito <alanbritodossantos2015@gmail.com>
2021-11-27 02:50:32 -0300

    foi criado um metodo e foi chamado pelo parte front

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,15 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
 })
 
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+
+  transferir() {
+    console.log('solicitada nova transferência');
+  }
 
 }
