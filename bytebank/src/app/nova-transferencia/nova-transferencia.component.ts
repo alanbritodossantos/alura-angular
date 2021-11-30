d40310fa9c2f601752290a3bac74e80d8d434f22
Alan Brito <alanbritodossantos2015@gmail.com>
2021-11-30 00:29:47 -0300

    Property Bind

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -8,8 +8,13 @@ import { Component } from "@angular/core";
 
 export class NovaTransferenciaComponent {
 
+valor: number;
+destino: number;
+
   transferir() {
     console.log('solicitada nova transferência');
+    console.log('Valor: ',this.valor);
+    console.log('Destino: ',this.destino);
   }
 
 }
