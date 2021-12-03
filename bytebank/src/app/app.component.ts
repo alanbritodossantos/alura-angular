6fbc4060465d524c9201eaa46157221c104644cb
Alan Brito <alanbritodossantos2015@gmail.com>
2021-12-02 23:28:11 -0300

    Criando primeiro service

--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,12 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bytebank';
 
-  transferencias:any[] = [];
+  constructor(private service: TransferenciaService){}
 
   transferir($event) {
-    console.log($event);
-    const transferencia = {...$event, data: new Date()};
-    this.transferencias.push(transferencia);
+    this.service.adicionar($event);
+
   }
 
 }
